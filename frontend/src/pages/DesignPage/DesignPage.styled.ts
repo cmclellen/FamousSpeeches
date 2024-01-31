fd4e839920139a05a57bd6b93c25c05136e94a49
acmac <craig.mclellen@eighty20solutions.com.au>
2024-01-31 18:51:19 +0800

    Fix

---
+++ frontend/src/pages/DesignPage/DesignPage.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const DesignPageWrapper = styled.div`
+`;
+
+export const Heading = styled.h4`
+`
--- frontend/src/pages/DesignPage/DesignPage.tsx
+++ frontend/src/pages/DesignPage/DesignPage.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react';
+import { FC } from "react";
+import flowpng from "../../assets/flow.png";
+import { DesignPageWrapper, Heading } from "./DesignPage.styled";
+import Loader from '../../assets/ask-question.mp4'
 
 interface DesignPageProps {}
 
 const DesignPage: FC<DesignPageProps> = () => (
-  <div>
-    DesignPage Component
-  </div>
+  <DesignPageWrapper>
+    <div className="container">
+      <Heading>Overview</Heading>
+      Retrieval Augmented Generation (RAG) builds on a LLM (Large Language Model) in a way that it references a specific knowledge base (famous speeches PDFs in this case) before returning a response, i.e. 
+      giving a chatbot relevant context of a particular subject that it'll use to generate a response to a user's questions with.
+      <Heading>Flowchart</Heading>
+      <img src={flowpng} alt="Flowchart" className="img-fluid" />
+      <Heading>Demo</Heading>
+      <video autoPlay loop muted>
+           <source src={Loader} type="video/mp4" />
+           Your browser does not support the video tag.
+      </video>
+    </div>
+  </DesignPageWrapper>
 );
 
 export default DesignPage;
